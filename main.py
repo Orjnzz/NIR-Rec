e5270d67c56d3fee5a3818fa61348706c0c0acc9
Orjnzz <ngochaoclhd@gmail.com>
2025-03-20 22:45:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -23,43 +23,39 @@ request_handler = Request(api_keys, model_name)
 data_ml_100k = read_json("./ml_100k.json")
 user_matrix_sim, item_matrix_sim, u_item_dict, i_item_dict, i_item_id_list = preprocess_data(data_ml_100k)
 
-# Variables
+
 length_limit = config['data']['length_limit']
 num_u = 12
 total_i = config['data']['num_candidates']
 random_seed = config['random_seed']
 results_data = []
-rank_list = []  # Danh sách để lưu rank của ground truth
+rank_list = []  
 
-# Candidate selection and  predictions
+
 for i, elem in enumerate(tqdm(data_ml_100k[:1000], desc="Processing")):
     seq_list = elem[0].split(' | ')
     candidate_items = sort_uf_items(seq_list, user_matrix_sim[i], num_u=num_u, num_i=total_i)
     input_1 = TEMPLATE_1.format(', '.join(candidate_items), ', '.join(seq_list[-length_limit:]))
 
-    # Step 1: Predict preferences
+    # Bước 1: Dự đoán sở thích
     predictions_1 = request_handler.request(user=input_1)
     input_2 = TEMPLATE_2.format(', '.join(candidate_items), ', '.join(seq_list[-length_limit:]), predictions_1)
 
-    # Step 2: Select top movies
+    # Bước 2: Chọn ra top movies
     predictions_2 = request_handler.request(user=input_2)
     input_3 = TEMPLATE_3.format(', '.join(candidate_items), ', '.join(seq_list[-length_limit:]), predictions_1, predictions_2)
 
-    # Step 3: Recommend movies
+    # Bước 3: Gợi ý movies
     predictions = request_handler.request(user=input_3)
     hit_ = elem[1] in predictions
 
-    # Calculate rank
     try:
-        rank = predictions.index(elem[1]) + 1  # Rank starts at 1
+        rank = predictions.index(elem[1]) + 1 
     except ValueError:
-        rank = len(predictions) + 1  # Nếu không có trong predictions, gán rank ngoài phạm vi
-
+        rank = len(predictions) + 1 
     rank_list.append(rank)
 
-    # Collect results
     results_data.append({
-        "PID": i,
         "Input_1": input_1,
         "Input_2": input_2,
         "Input_3": input_3,
@@ -71,10 +67,8 @@ for i, elem in enumerate(tqdm(data_ml_100k[:1000], desc="Processing")):
         "Rank": rank
     })
 
-# Kiểm tra và tạo thư mục 'results' nếu chưa tồn tại
 os.makedirs("./results", exist_ok=True)
 
-# Lưu kết quả chi tiết vào CSV
 results_df = pd.DataFrame(results_data)
 results_df["Hit"] = results_df["Hit"].astype(int)
 results_df.to_csv(f"./results/results_len{length_limit}_cand{total_i}_seed{random_seed}.csv", index=False)
